869fb5e28439eb5476840d73f5dcdcaf2ef4aec1
Arlak <arlak.a@orcsoft.co.th>
2023-05-11 00:24:35 +0700

    finish customer repository

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bank/repository"
+	"fmt"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	db, err := sqlx.Open("mysql", "root:admin123@tcp(127.0.0.1:3306)/bank")
+
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	customerRepository := repository.NewCustomerRepositoryDb(db)
+	_ = customerRepository
+	// customers, err := customerRepository.GetAll()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(customers)
+
+	customer, err := customerRepository.GetById(2000)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
 }
--- repository/customer.go
+++ repository/customer.go
@@ -1,5 +1,14 @@
 package repository
 
-type CustomerRepositoey interface {
-	GetAll()
+type Customer struct {
+	CustomerID  int    `db:"customer_id"`
+	Name        string `db:"name"`
+	DateOfBirth string `db:"date_of_birth"`
+	City        string `db:"city"`
+	Zipcode     string `db:"zipcode"`
+	Status      int    `db:"status"`
+}
+type CustomerRepository interface {
+	GetAll() ([]Customer, error)
+	GetById(id int) (*Customer, error)
 }
